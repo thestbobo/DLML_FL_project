7aba73bb81f8d8c208fbe43dc7378a87db909f39
thestbobo <bobbiplus2002@icloud.com>
2025-04-16 12:02:37 +0200

    vit_b replaced with vit_s

--- models/dino_ViT_b16.py
+++ models/dino_ViT_b16.py
@@ -7,10 +7,10 @@ class DINO_ViT(nn.Module):
     def __init__(self, num_classes=100, pretrained=True):
         super(DINO_ViT, self).__init__()
         self.model = torch.hub.load('facebookresearch/dino:main', 'dino_vits16')
-        # self.model = vit_b_16(weights='DEFAULT' if pretrained else None)
-        in_features = self.model.heads.head.in_features
-        self.model.heads.head = nn.Linear(in_features, num_classes)
+        self.classifier = nn.Linear(384, num_classes)
 
     def forward(self, x):
-        return self.model(x)
+        x = self.model(x)
+        x = self.classifier(x)
+        return x
 
