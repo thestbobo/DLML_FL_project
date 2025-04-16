3ec40b47900794844a1103c28c9746830ca1303e
thestbobo <bobbiplus2002@icloud.com>
2025-04-16 11:32:58 +0200

    vit_b replaced with vit_s

--- models/dino_ViT_b16.py
+++ models/dino_ViT_b16.py
@@ -1,13 +1,14 @@
 import torch.nn as nn
 import torch
 
-from torchvision.models import vit_b_16        # might have to manually download from the GitHub repo (facebook research) the original DINOViT weights
+from torchvision.models import ViT_B_16_Weights     # might have to manually download from the GitHub repo (facebook research) the original DINOViT weights
 
 class DINO_ViT(nn.Module):
     def __init__(self, num_classes=100, pretrained=True):
         super(DINO_ViT, self).__init__()
-        self.model = vit_b_16(weights='DEFAULT' if pretrained else None)
-        in_features = self.model.heads.head.in_features
+        self.model = torch.hub.load('facebookresearch/dino:main', 'dino_vits16')
+        # self.model = vit_b_16(weights='DEFAULT' if pretrained else None)
+        in_features = self.model.head.in_features
         self.model.heads.head = nn.Linear(in_features, num_classes)
 
     def forward(self, x):
