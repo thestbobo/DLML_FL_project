ba594c096d4c65c5cc592e253fd0ecc9dc2b260c
thestbobo <bobbiplus2002@icloud.com>
2025-05-16 16:52:02 +0200

    correction of < in mask calibration

--- noob.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-checkpoint = torch.load("/Users/gabrieleadorni/Downloads/checkpoint_15.pth", map_location="cpu")
-
-print(checkpoint.keys())
-print(checkpoint["train_metrics"])
-print(checkpoint["val_loss"])
-print(checkpoint["train_loss"])
